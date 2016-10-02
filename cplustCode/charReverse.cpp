8767205be54876f5ad5ef884933364b4a2da2263
Xwei <xwei@xweideMacBook-Pro.local>
2016-10-02 11:17:01 +0800

    add myfun strlen && reverse for char in c++

---
+++ cplustCode/charReverse.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int myStrLen(const char *str){
+	int ret = 0;
+	if(str != NULL){
+		while(*str != '\0'){
+			ret++;
+			str++;
+		}
+	}
+	return ret;
+}
+char* CharReverse(char* pch,int index, int length){
+	if(index < length/2){
+		char tmp = *(pch+index);
+		*(pch + index) = *(pch + length - (index+1));
+		*(pch + length - (index+1)) = tmp;
+		cout << tmp << endl;
+		return CharReverse(pch, ++index, length);
+	}
+	else{
+		return pch;
+	}
+}
+int main(){
+	char cp[] = "This is a test!";
+	cout << cp << endl;
+	int ilen = 0;
+	ilen = myStrLen(cp);
+	cout << ilen << endl;
+	ilen = strlen(cp);
+	cout << ilen << endl;
+	int sp = 0; 
+	CharReverse(cp, sp, ilen);
+
+	cout << cp << endl;
+	return 0;		
+}
+
